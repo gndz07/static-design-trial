49a71c0f8d3a6481e3f940a2049ddb374ae5cb61
Gina A <ginadzani@gmail.com>
2021-03-24 00:55:53 +0100

    Add validation for name and email

--- index.js
+++ index.js
@@ -1,19 +1,25 @@
 //form focus event
 //input field DOM
 const moveLegendField = Array.from(document.querySelectorAll("input.form-data"));
-console.log(moveLegendField);
 
 //show label on top when focused
 moveLegendField.forEach((field) => {
-	field.addEventListener("focus", (e) => {
+	const moveLegend = (e) => {
 		e.target.removeAttribute("placeholder");
 		e.target.previousElementSibling.style.display = "block";
-	})
+		e.target.parentElement.style.padding = "0.6em";
+	}
+	field.addEventListener("focus", moveLegend);
 
 	field.addEventListener("blur", (e) => {
-		const legend = e.target.previousElementSibling;
-		legend.style.display = "none";
-		e.target.setAttribute("placeholder", legend.innerHTML);
+		if (e.target.value.length == 0) {
+			const legend = e.target.previousElementSibling;
+			legend.style.display = "none";
+			e.target.setAttribute("placeholder", legend.innerHTML);
+			e.target.parentElement.style.padding = "0.8em";
+		} else {
+			moveLegend(e);
+		}	
 	})
 })
 
@@ -21,4 +27,54 @@ moveLegendField.forEach((field) => {
 //take all input field to verify
 const formInput = Array.from(document.querySelectorAll(".create-form__data__input"));
 
+//DOM for all input fields
+const name = document.getElementById("name-input");
+const email = document.getElementById("email-input");
+const userType = document.getElementById("user-type-input");
+const password = document.getElementById("password-input");
 
+//function to add attribute to show error message
+const showError = (index, message) => {
+	formInput[index].setAttribute("data-error", message);
+	formInput[index].setAttribute("data-error-visible", true);
+};
+
+//function to hide error when input is valid
+const hideError = (index) => {
+	formInput[index].removeAttribute("data-error");
+	formInput[index].removeAttribute("data-error-visible");
+};
+
+//validation functions
+let valid = true;
+
+//name validation
+const validateName = () => {
+	const nameRegex = /^[A-Za-z]+$/;
+	if (!nameRegex.test(name.value) && name.value.length > 0) {
+ 		valid = false;
+ 		message = "Please enter your name";
+		showError(0, message);		
+ 	} else {
+ 		valid;
+ 		hideError(0);	
+ 	}
+ 	return valid;
+};
+name.addEventListener("blur", validateName);
+
+
+//email validation
+const validateEmail = () => {
+	const regexEmail = /.+@.+\..+/;
+ 	if (!regexEmail.test(email.value)) {
+ 		valid = false;
+ 		message = "Please enter a valid email address";
+ 		showError(1, message);
+ 	} else {
+ 		valid;
+ 		hideError(1);
+ 	}
+ 	return valid;
+};
+email.addEventListener("blur", validateEmail);
