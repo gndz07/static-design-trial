e52a2105da676123562813a9e84e0f2400156dbe
Gina A <ginadzani@gmail.com>
2021-03-24 11:42:13 +0100

    Fix validation function

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ const hideError = (index) => {
 };
 
 //validation functions
-let valid = true;
+let valid;
 
 //name validation
 const validateName = () => {
@@ -56,25 +56,83 @@ const validateName = () => {
  		message = "Please enter your name";
 		showError(0, message);		
  	} else {
- 		valid;
+ 		valid = true;
  		hideError(0);	
  	}
  	return valid;
 };
-name.addEventListener("blur", validateName);
+//name.addEventListener("blur", validateName);
 
 
 //email validation
 const validateEmail = () => {
 	const regexEmail = /.+@.+\..+/;
- 	if (!regexEmail.test(email.value)) {
+ 	if (!regexEmail.test(email.value) && email.value.length > 0) {
  		valid = false;
  		message = "Please enter a valid email address";
  		showError(1, message);
  	} else {
- 		valid;
+ 		valid = true;
  		hideError(1);
  	}
  	return valid;
 };
-email.addEventListener("blur", validateEmail);
+//email.addEventListener("blur", validateEmail);
+
+const validateUserType = () => {
+	if (userType.value.length < 1) {
+		valid = false;
+	} else {
+		valid = true;
+	}
+	return valid;
+}
+
+//password validation
+const validatePassword = () => {
+	if (password.value.length < 8 && password.value.length > 0) {
+		valid = false;
+ 		message = "Password must be at least 8 characters";
+ 		showError(3, message);
+	} else {
+ 		valid = true;
+ 		hideError(3);
+ 	}
+ 	return valid;
+};
+//password.addEventListener("blur", validatePassword);
+
+const validate = () => {
+	let valid;
+	let nameValid = validateName();
+	let emailValid = validateEmail();
+	let typeValid = validateUserType();
+	let passwordValid = validatePassword();
+
+	if (name.value.length > 0 && nameValid == true && 
+		email.value.length > 0 && emailValid == true && 
+		typeValid == true &&
+		password.value.length > 0 && passwordValid == true) {
+		valid = true;
+	} else {
+		valid = false;
+	}
+	return valid;
+}
+
+const form = document.getElementById("create-form");
+const submitBtn = document.getElementById("submit-btn");
+
+form.addEventListener("keyup", (e) => {
+	let result = validate();
+	
+	if (result == true) {
+		submitBtn.removeAttribute("disabled");
+		submitBtn.setAttribute("aria-disabled", false);
+		submitBtn.classList.add("active-btn");
+	} else {
+		submitBtn.disabled = true;
+		submitBtn.setAttribute("aria-disabled", true);
+		submitBtn.classList.remove("active-btn");
+	}
+})
